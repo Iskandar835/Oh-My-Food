1909f05c7af2a78c0b47792bbd2858f682b49e2d
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-07 13:47:01 +0100

    Architecture dossiers optimiser/ Ajout des utils avec légère correction de ceux-ci/ Ajout Header et Footer sans optimisation

--- refont/src/main.jsx
+++ refont/src/main.jsx
@@ -1,4 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
