7da9ad36fa0bbf7682ba263b3eae90afe949cefd
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-08 22:41:33 +0100

    Ajout du global style/ Ajout de la homepage/ Ajout font in index.html

--- refont/src/main.jsx
+++ refont/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import "./utils/globalStyle.scss";
+import Homepage from "./pages/Homepage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </Router>
   </StrictMode>
